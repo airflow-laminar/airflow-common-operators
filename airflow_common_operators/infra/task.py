bdcd4a2a9c2b3a75047e363fa15673cb84d8ab0b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-07-05 14:10:33 -0400

    Make journalctl bash task, add ssh task

--- airflow_common_operators/infra/task.py
+++ airflow_common_operators/infra/task.py
@@ -1,10 +1,21 @@
 from typing import Optional, Type
 
-from airflow_pydantic import CallablePath, Task, TaskArgs
-from airflow_pydantic.airflow import PythonOperator
+from airflow_pydantic import BashTask, BashTaskArgs, CallablePath, SSHTask, SSHTaskArgs
+from airflow_pydantic.airflow import BashOperator, SSHOperator
 from pydantic import Field, field_validator
 
-__all__ = ("JournalctlClean", "JournalctlCleanOperator", "JournalctlCleanOperatorArgs", "JournalctlCleanTask", "JournalctlCleanTaskArgs")
+__all__ = (
+    "JournalctlClean",
+    "JournalctlCleanSSH",
+    "JournalctlCleanOperator",
+    "JournalctlCleanOperatorArgs",
+    "JournalctlCleanSSHOperatorArgs",
+    "JournalctlCleanSSHOperator",
+    "JournalctlCleanTask",
+    "JournalctlCleanTaskArgs",
+    "JournalctlCleanSSHTask",
+    "JournalctlCleanSSHTaskArgs",
+)
 
 
 def clean_journalctl(sudo: Optional[bool] = True, days: Optional[int] = 2):
@@ -13,23 +24,36 @@ def clean_journalctl(sudo: Optional[bool] = True, days: Optional[int] = 2):
     return cmd
 
 
-class JournalctlClean(PythonOperator):
+class JournalctlClean(BashOperator):
     def __init__(self, sudo: Optional[bool] = True, days: Optional[int] = 2, **kwargs):
-        if "python_callable" in kwargs:
-            raise ValueError("JournalctlClean does not accept 'python_callable' as an argument.")
-        super().__init__(python_callable=lambda: clean_journalctl(sudo=sudo, days=days), **kwargs)
+        if "bash_command" in kwargs:
+            raise ValueError("JournalctlClean does not accept 'bash_command' as an argument.")
+        super().__init__(bash_command=clean_journalctl(sudo=sudo, days=days), **kwargs)
 
 
-class JournalctlCleanTaskArgs(TaskArgs):
+class JournalctlCleanSSH(SSHOperator):
+    def __init__(self, sudo: Optional[bool] = True, days: Optional[int] = 2, **kwargs):
+        if "command" in kwargs:
+            raise ValueError("JournalctlCleanSSH does not accept 'command' as an argument.")
+        super().__init__(command=clean_journalctl(sudo=sudo, days=days), **kwargs)
+
+
+class JournalctlCleanTaskArgs(BashTaskArgs):
+    sudo: Optional[bool] = Field(default=True)
+    days: Optional[int] = Field(default=2)
+
+
+class JournalctlCleanSSHTaskArgs(SSHTaskArgs):
     sudo: Optional[bool] = Field(default=True)
     days: Optional[int] = Field(default=2)
 
 
 # Alias
 JournalctlCleanOperatorArgs = JournalctlCleanTaskArgs
+JournalctlCleanSSHOperatorArgs = JournalctlCleanSSHTaskArgs
 
 
-class JournalctlCleanTask(Task, JournalctlCleanTaskArgs):
+class JournalctlCleanTask(BashTask, JournalctlCleanTaskArgs):
     operator: CallablePath = Field(default="airflow_common_operators.JournalctlClean", validate_default=True)
 
     @field_validator("operator")
@@ -40,5 +64,17 @@ class JournalctlCleanTask(Task, JournalctlCleanTaskArgs):
         return v
 
 
+class JournalctlCleanSSHTask(SSHTask, JournalctlCleanSSHTaskArgs):
+    operator: CallablePath = Field(default="airflow_common_operators.JournalctlCleanSSH", validate_default=True)
+
+    @field_validator("operator")
+    @classmethod
+    def validate_operator(cls, v: Type) -> Type:
+        if v is not JournalctlCleanSSH:
+            raise ValueError(f"operator must be 'airflow_common_operators.JournalctlCleanSSH', got: {v}")
+        return v
+
+
 # Alias
 JournalctlCleanOperator = JournalctlCleanTask
+JournalctlCleanSSHOperator = JournalctlCleanSSHTask
